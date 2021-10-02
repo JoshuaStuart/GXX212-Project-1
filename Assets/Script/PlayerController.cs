1bc6f6c03cdfe49dd79eab34c60b13e95ab8afbd
BJNicholas <85209940+BJNicholas@users.noreply.github.com>
2021-10-02 15:00:31 +1000

    added proof of concept to show

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,6 +8,12 @@ public class PlayerController : MonoBehaviour
     public Transform spawnPoint;
     float x,y;
 
+    GameObject controlledBubble;
+
+    private void Start()
+    {
+        newControlledBubble();
+    }
 
     private void FixedUpdate()
     {
@@ -18,12 +24,13 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         gameObject.transform.position += new Vector3(x * moveSpeed * Time.deltaTime, 0, y * moveSpeed * Time.deltaTime);
-
+        controlledBubble.transform.position = spawnPoint.position;
 
         if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             GameManager.instance.turnsRemaining -= 1;
-
+            controlledBubble.GetComponent<BallScript>().gravity = 10;
+            controlledBubble.GetComponent<SphereCollider>().enabled = true;
             if(GameManager.instance.turnsRemaining > 0)
             {
                 newControlledBubble();
@@ -35,6 +42,9 @@ public class PlayerController : MonoBehaviour
     {
         GameObject newBubble = Instantiate(GameManager.instance.bubble);
         newBubble.transform.position = spawnPoint.position;
-        newBubble.GetComponent<SphereCollider>().enabled = true;
+        newBubble.GetComponent<SphereCollider>().enabled = false;
+        newBubble.GetComponent<BallScript>().gravity = 0;
+
+        controlledBubble = newBubble;
     }
 }
